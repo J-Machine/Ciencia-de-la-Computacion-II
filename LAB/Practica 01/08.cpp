ad2fcb29c9202a40d1495837b09e0ae3cadfd697
Jenny Ha <jhuancaan@unsa.edu.pe>
2021-04-23 12:08:43 -0500

    Advance

--- LAB/Practica 01/08.cpp
+++ LAB/Practica 01/08.cpp
@@ -1,5 +1,6 @@
 /*
-    
+    Los cuatro dígitos adyacentes en el número de 1000 dígitos que tienen el mayor producto son 9 × 9 × 8 × 9 = 5832.
+    Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 */
 #include <iostream>
 #include <cmath>
--- LAB/Practica 01/09.cpp
+++ LAB/Practica 01/09.cpp
@@ -1,13 +1,27 @@
 /*
-    
+    Un triplete pitagórico es un conjunto de tres números naturales, a 
+    Existe exactamente un triplete pitagórico para el cual a + b + c = 1000.
+    Encuentre el producto abc.
 */
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-
 int main()
 {
-    
+    int a,b,c;
+    for (a = 1; a < 1000; a ++)
+    {
+        for (b = a +1; b < 1000; b++)
+        {
+            c = 1000-(a+b);
+            if (pow(a,2) + pow(b,2) == pow(c,2))
+            {
+                cout<<"a,b,c : "<<a<<"-"<<b<<"-"<<c<<endl;
+                cout<<"abc : "<<(a*b*c)<<endl;
+            }
+        }
+    }
     return 0;
+    
 }

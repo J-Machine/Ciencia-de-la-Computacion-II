78b546e1f76da64e992e52ab485fb7716a694c1a
Jenny Ha <jhuancaan@unsa.edu.pe>
2021-05-05 10:33:25 -0500

    update

---
+++ LAB/Practica 02/02.cpp
@@ -0,0 +1,40 @@
+/* 
+* Implementar una función que invierta los elementos de un arreglo de enteros (Iterativa y recursiva). Tamaño del
+    arreglo 1000000.
+*/
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+void print (long* arr, long tam)
+{
+    while(tam--)    // cuando tam = 0 e detiene
+        cout<< *arr++<< " ";
+    cout<<endl;
+}
+void swap( int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void invertir (long* start, long* end)
+{
+    while(start < end)
+        swap(*start++, *end--);        
+} 
+int main ()
+{
+    // Creación de un array de los 1 000 000 primeros numeros enteros
+    long n = 1000000;
+    long arrayN[n];
+    for (long i=1; i<=n; i++)
+        arrayN[i] = i;
+    
+    invertir(arrayN, arrayN + n);
+    print(arrayN, n);
+    return 0;
+}
